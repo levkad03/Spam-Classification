9234467f6dd4df7c1540f65b2da112d55946e900
Lev Kadatskyi <levkadatscky@gmail.com>
2025-06-13 15:11:15 +0300

    added config of the model

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    model_name: str = "bert-base-uncased"
+    max_length: int = 128
+    batch_size: int = 32
+    epochs: int = 20
+    learning_rate: float = 2e-5
+    output_dir: str = "models/bert_spam_model"
